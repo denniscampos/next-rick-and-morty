753a819a9d33b91e717cf2a2edf965b5590a17fe
denniscampos <dennisjr89@gmail.com>
2021-12-25 00:21:54 -0500

    added some reset css to start with styling on pages.

--- pages/character/[id]/index.tsx
+++ pages/character/[id]/index.tsx
@@ -1,64 +1,61 @@
-import Image from "next/image"
-import {
-    ApolloClient,
-    InMemoryCache,
-    gql
-  } from "@apollo/client";
-import {useRouter} from "next/router"
+import Image from "next/image";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { useRouter } from "next/router";
 
 interface Props {
-    character: any,
-    id: number,
-    name: string,
-    image: string,
-    status: string,
-    species: string,
-    gender: string
+  character: any;
+  id: number;
+  name: string;
+  image: string;
+  status: string;
+  species: string;
+  gender: string;
 }
 
 interface Character {
-    id: number,
+  id: number;
 }
 
 interface Context {
-    params: {
-      id: number
-    }
-  }
-
-const Character = ({ character }: Props) => {
-    const router = useRouter()
-    console.log('router', router.query)
-    return (
-        <div>
-            <p>{character.name}</p>
-            <p>{character.status}</p>
-            <p>{character.species}</p>
-            <p>{character.gender}</p>
-            {/* <Image src={character.image} width={300} height={200}/> */}
-        </div>
-    )
+  params: {
+    id: number;
+  };
 }
 
-export default Character
+const Character = ({ character }: Props) => {
+  const router = useRouter();
+  console.log("router", router.query);
+  return (
+    <div>
+      <p>{character.name}</p>
+      <p>{character.status}</p>
+      <p>{character.species}</p>
+      <p>{character.gender}</p>
+      <Image src={character.image} width={300} height={200} />
+    </div>
+  );
+};
+
+export default Character;
 
 export async function getStaticPaths() {
-    const res = await fetch(`https://rickandmortyapi.com/api/character`)
-    const characters = await res.json()
-    console.log('check here', characters.results)
-    const paths = characters.results.map((character: Character) => ({params: { id: character.id.toString() }}))
-    // console.log('check params', paths)
-    return {paths, fallback: 'blocking'}
+  const res = await fetch(`https://rickandmortyapi.com/api/character`);
+  const characters = await res.json();
+  const paths = characters.results.map((character: Character) => ({
+    params: { id: character.id.toString() },
+  }));
+
+  return { paths, fallback: "blocking" };
 }
 
 export async function getStaticProps(context: Context) {
-    const client = new ApolloClient({
-      uri: 'https://rickandmortyapi.com/graphql',
-      cache: new InMemoryCache()
-    })
-  
-    const { data } = await client.query({
-      query: gql`
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
       query {
         character(id: ${context.params.id}) {
           id
@@ -69,12 +66,11 @@ export async function getStaticProps(context: Context) {
           image
         }
       }
-      `
-    })
-//   console.log('character data', data)
-    return {
-      props: {
-        character: data.character
-      }
-    }
-  }
+      `,
+  });
+  return {
+    props: {
+      character: data.character,
+    },
+  };
+}
--- pages/location/[id]/index.tsx
+++ pages/location/[id]/index.tsx
@@ -1,70 +1,69 @@
-import Image from "next/image"
-import Link from "next/link"
-import {
-    ApolloClient,
-    InMemoryCache,
-    gql
-  } from "@apollo/client";
+import Image from "next/image";
+import Link from "next/link";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 interface Props {
-  residents: Props[]
-  name: string,
-  image: string,
-  status: string,
-  species: string,
-  gender: string
-  id: number
+  residents: Props[];
+  name: string;
+  image: string;
+  status: string;
+  species: string;
+  gender: string;
+  id: number;
 }
 
 interface Context {
   params: {
-    id: number
-  }
+    id: number;
+  };
 }
 
 interface Locations {
-  id: number,
+  id: number;
 }
 
-const location = ({residents}: Props) => {
-    return (
-        <div>
-            <h1>location</h1>
-            <h3>Residents</h3>
-            {residents.map((resident, i) => (
-              <Link href={`/character/${resident.id}`} key={resident.id}>
-              <div key={i}>
-                <h3>{resident.name}</h3>
-                <span>{resident.status}</span>
-                <span>{resident.species}</span>
-                <span>{resident.gender}</span>
-                <Image src={resident.image} width={300} height={200}/>
-              </div>
-              </Link>
-            ))}
-        </div>
-    )
-}
+const location = ({ residents }: Props) => {
+  return (
+    <div>
+      <h1>location</h1>
+      <h3>Residents</h3>
+      {residents.map((resident) => (
+        <Link href={`/character/${resident.id}`} key={resident.id}>
+          <a>
+            <div>
+              <h3>{resident.name}</h3>
+              <span>{resident.status}</span>
+              <span>{resident.species}</span>
+              <span>{resident.gender}</span>
+              <Image src={resident.image} width={300} height={200} />
+            </div>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
-export default location
+export default location;
 
 export async function getStaticPaths() {
-  const res = await fetch(`https://rickandmortyapi.com/api/location`)
+  const res = await fetch(`https://rickandmortyapi.com/api/location`);
 
-  const locations = await res.json()
-  const paths = locations.results.map((location: Locations) => ({params: { id: location.id.toString() }}))
-  return {paths, fallback: 'blocking'}
+  const locations = await res.json();
+  const paths = locations.results.map((location: Locations) => ({
+    params: { id: location.id.toString() },
+  }));
+  return { paths, fallback: "blocking" };
 }
 
-
 export async function getStaticProps(context: Context) {
-    const client = new ApolloClient({
-      uri: 'https://rickandmortyapi.com/graphql',
-      cache: new InMemoryCache()
-    })
-  
-    const { data } = await client.query({
-      query: gql`
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
       query {
         location(id: ${context.params.id}) {
           id
@@ -80,12 +79,12 @@ export async function getStaticProps(context: Context) {
           }
         }
       }
-      `
-    })
-  
-    return {
-      props: {
-        residents: data.location.residents
-      }
-    }
-  }
+      `,
+  });
+
+  return {
+    props: {
+      residents: data.location.residents,
+    },
+  };
+}
