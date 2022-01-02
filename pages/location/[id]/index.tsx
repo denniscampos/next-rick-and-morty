94ca567996027a66e5734373cc89c23b2780ec53
Dennis Campos <dennisjr89@gmail.com>
2022-01-01 21:37:15 -0500

    added search results for characters when inside a location

--- pages/location/[id]/index.tsx
+++ pages/location/[id]/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { gql } from "@apollo/client";
 import { GetStaticProps, GetStaticPaths } from "next";
@@ -21,7 +22,8 @@ interface Locations {
   id: number;
 }
 
-const location = ({ residents }: Props) => {
+const Location = ({ residents }: Props) => {
+  const [search, setSearch] = useState("");
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
 
@@ -31,33 +33,49 @@ const location = ({ residents }: Props) => {
       <div className={styles.goBack__btn}>
         <button onClick={() => router.back()}>Go Back</button>
       </div>
+      <div className={styles.search__character}>
+        <input
+          type="text"
+          placeholder="Search Character..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <main className={styles.resident__container}>
-        {residents.map((resident) => (
-          <Link href={`/character/${resident.id}`} key={resident.id}>
-            <a>
-              <section className={styles.resident__container__card}>
-                <Image
-                  src={resident.image}
-                  alt="character profile card"
-                  width={300}
-                  height={200}
-                />
-                <div className={styles.resident__container__card__information}>
-                  <h3>{resident.name}</h3>
-                  <span>{resident.status}</span>
-                  <span>{resident.species}</span>
-                  <span>{resident.gender}</span>
-                </div>
-              </section>
-            </a>
-          </Link>
-        ))}
+        {residents
+          .filter((resident) => {
+            const fullName = `${resident.name}`;
+            if (search === "") return resident;
+            else if (fullName.toLowerCase().includes(search.toLowerCase()))
+              return resident;
+          })
+          .map((resident) => (
+            <Link href={`/character/${resident.id}`} key={resident.id}>
+              <a>
+                <section className={styles.resident__container__card}>
+                  <Image
+                    src={resident.image}
+                    alt="character profile card"
+                    width={300}
+                    height={200}
+                  />
+                  <div
+                    className={styles.resident__container__card__information}
+                  >
+                    <h3>{resident.name}</h3>
+                    <span>{resident.status}</span>
+                    <span>{resident.species}</span>
+                    <span>{resident.gender}</span>
+                  </div>
+                </section>
+              </a>
+            </Link>
+          ))}
       </main>
     </div>
   );
 };
 
-export default location;
+export default Location;
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const res = await fetch(`https://rickandmortyapi.com/api/location`);
